d8dea9c4478047cdf4beb514e5a79e896bf85faf
Ghada-Ch <57904143+Ghada-Ch@users.noreply.github.com>
2020-02-26 17:01:19 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 <?php
- $url = "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22?id=ddd472af7108b851e7bc8caafeb9596e";
-    $raw =file_get_contents($url);
+$url="api_url&appid=your_api_key";
+ $raw =file_get_contents($url);
     $json = json_decode($raw);
 
 
