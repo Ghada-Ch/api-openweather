c904ed110e92b780df8d279433384bcc3b99a56f
Ghada-Ch <57904143+Ghada-Ch@users.noreply.github.com>
2020-02-28 18:00:23 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,17 +5,29 @@ $url="api_url&appid=your_api_key";
  $raw =file_get_contents($url);
     $json = json_decode($raw);
 
+echo "<table>";
+if(!empty($json->list)) {
 
-if(!empty($json->groups)) {
+echo "<tr><th>Time</th><th>Pressure</th><th>Degree (F)</th>";
 foreach($json->list as $msg) {
-    echo "<u>" . $msg->id ."</u> : ". $msg->name;
+
+    echo "<tr><td>" .$msg->dt_txt. "</td><td>" . $msg->main->pressure ." </td><td> ". $msg->wind->deg."</td></tr>";
     echo "<br />";
 }
+
 }else {
     echo "Rien n'a &eacute;t&eacute; trou&eacute;.";
 }
-
+echo "</table>";
 ?>
-
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+table{
+top:3em;
+position:absolute;
+}
+</style>
 </body>
 </html>
